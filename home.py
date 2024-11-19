be29c98f3601d2d9e162b780bc6742f8719dd519
Simpp23 <nattawat71710@gmail.com>
2024-11-19 14:47:50 +0700

    update

--- home.py
+++ home.py
@@ -4,6 +4,9 @@ import pandas as pd
 st.title("🥨🥨WedSite Developing using Python🥨🥨")
 st.header("🥪WedSite Developing using Python🥪")
 
+st.image('./img/IMG_2571.jpg')
 st.subheader("Nattawat MTV🧸🎧🎸")
 
-
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.warning(dt.head(10))
